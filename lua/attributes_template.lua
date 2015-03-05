8ccce36909197765d11e2e0984dc91d8a405d757
Stephan Lesch <SLesch@hilscher.com>
2015-03-05 15:30:00 +0100

    corrected typo

--- lua/attributes_template.lua
+++ lua/attributes_template.lua
@@ -1,8 +1,8 @@
 local atSdramAttributes = {
-	netX.          = ${CHIP_TYPE},
-	general_ctrl.  = ${REGISTER_GENERAL_CTRL},
-	timing_ctrl.   = ${REGISTER_TIMING_CTRL},
-	mr.            = ${REGISTER_MODE},
-	size_exponent. = ${SIZE_EXPONENT},
-	interface.     = ramtest.SDRAM_INTERFACE_${INTERFACE}
+	netX          = ${CHIP_TYPE},
+	general_ctrl  = ${REGISTER_GENERAL_CTRL},
+	timing_ctrl   = ${REGISTER_TIMING_CTRL},
+	mr            = ${REGISTER_MODE},
+	size_exponent = ${SIZE_EXPONENT},
+	interface     = ramtest.SDRAM_INTERFACE_${INTERFACE}
 }
