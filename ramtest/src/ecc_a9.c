a25da6c71c0049c0330e51406d4628e261d7ac92
dizzydevil <stephan_lesch@users.sourceforge.net>
2017-03-30 17:01:55 +0200

    Extended L1 cache ECC test to work on both A9 cores.

--- ramtest/src/ecc_a9.c
+++ ramtest/src/ecc_a9.c
@@ -70,6 +70,8 @@ int ca9_ecc_ctrl_check_print(void) {
 	return iRet;
 }
 
+
+
 void ca9_ecc_ctrl_show(void) {
 	ca9_ecc_ctrl_check_print();
 }
@@ -102,91 +104,164 @@ int ca9_l1_ecc_ctrl_trigger_errors(void){
 	unsigned int uiCPUNo;
 	int iRes;
 	
+	NX4000_DEF_ptCA9_EccCtrl;
+	unsigned long ulAddr;
+	volatile unsigned long *pulAddr;
+	unsigned long ulWriteVal;
+	unsigned long ulReadVal;
+	int i;
+
+	volatile unsigned long *pulStart;
+	volatile unsigned long *pulEnd;
+	volatile unsigned long *pulCnt;
+	
+	iRes = 0;
 	uiCPUNo = ramtest_get_cpuid(); 
+	
+	uprintf("\n");
+	uprintf("CA9 core %d L1 ECC error test\n", uiCPUNo);
+	
 	if (uiCPUNo == 0)
 	{
-		NX4000_DEF_ptCA9_EccCtrl;
-		unsigned long ulAddr;
-		volatile unsigned long *pulAddr;
-		unsigned long ulWriteVal;
-		unsigned long ulReadVal;
-		int i;
-
-		volatile unsigned long *pulStart;
-		volatile unsigned long *pulEnd;
-		volatile unsigned long *pulCnt;
-		
 		ulAddr = 0x07040000UL;
 		ulWriteVal = 0x12345678UL;
-		pulAddr = (volatile unsigned long *) ulAddr;
-		
-		//ca9_ecc_ctrl_clear();
-		uprintf("CA9 L1 ECC: triggering errors\n");
-		
-		*pulAddr = ulWriteVal;
-		
-		__asm__("dsb");
-		
-		for (i=1; i<8192; i++) 
-		{
-			pulAddr[i] = (unsigned long) i;
-		}
-		
-		__asm__("dsb");
-		
-		/*for (i=1; i<8192; i++) 
-		{
-			ulReadVal = pulAddr[i];
-		}*/
-		
-		/* preload dcache */
-		pulStart = pulAddr;
-		pulEnd = pulStart + 8192;
-		for (pulCnt=pulStart; pulCnt<pulEnd; pulCnt+=4)
-		{
-			__asm__("pld [%[ptr], #0]\n\t" : : [ptr] "r" (pulCnt) :);
-		}
-		
-		__asm__("dsb");
-		
-		/* setting/clearing bit 0 makes no visible difference to the value read */
-		ptCA9_EccCtrl->ulCPU0_DDATA = 0x3;
-		//ptCA9_EccCtrl->ulCPU0_DTAG = 0x3;
-		
-		__asm__("dsb");
-		
-		ulReadVal = *pulAddr;
-		
-		__asm__("dsb");
-		
-		ptCA9_EccCtrl->ulCPU0_DDATA = 0;
-		ptCA9_EccCtrl->ulCPU0_DTAG = 0;
-		
-		__asm__("dsb");
-		
-		iRes = ca9_ecc_ctrl_check_print();
-		
-		uprintf("addr: 0x%08x  written: 0x%08x  read: 0x%08x\n", ulAddr, ulWriteVal, ulReadVal);
-		
-		ca9_ecc_ctrl_clear();
-		
 	}
 	else
 	{
-		//ulAddr = 0x05048000;
-		//ulWriteVal = 0x44;
-		iRes = 1;
+		ulAddr = 0x07050000UL;
+		ulWriteVal = 0x87654321UL;
 	}
+	
+	pulAddr = (volatile unsigned long *) ulAddr;
+	*pulAddr = ulWriteVal;
+	__asm__("dsb");
+	
+	for (i=1; i<8192; i++) 
+	{
+		pulAddr[i] = (unsigned long) i;
+	}
+	__asm__("dsb");
+	
+	/* preload dcache */
+	pulStart = pulAddr;
+	pulEnd = pulStart + 8192;
+	
+	for (pulCnt=pulEnd-4; pulCnt>=pulStart; pulCnt-=4)
+	{
+		__asm__("pld [%[ptr], #0]\n\t" : : [ptr] "r" (pulCnt) :);
+	}
+	__asm__("dsb");
+		
+	if (uiCPUNo == 0)
+	{
+		/* clear error flags */
+		uprintf("Clearing CA9 core0 L1 ECC error flags before test\n");
+		ptCA9_EccCtrl->ulCPU0_SBE_INT = ptCA9_EccCtrl->ulCPU0_SBE_INT;
+		ptCA9_EccCtrl->ulCPU0_DBE_INT = ptCA9_EccCtrl->ulCPU0_DBE_INT;
+		__asm__("dsb");
+		
+		if ((ptCA9_EccCtrl->ulCPU0_SBE_INT != 0) || (ptCA9_EccCtrl->ulCPU0_DBE_INT != 0))
+		{
+			uprintf("Could not clear CA9 core 0 L1 ECC error flags before test\n");
+			iRes = 0;
+		}
+		else
+		{
+			/* set syndrome bits */
+			/* setting/clearing bit 0 (dec_bypass) makes no visible difference to the value read */
+			ptCA9_EccCtrl->ulCPU0_DDATA = 0x3;
+			//ptCA9_EccCtrl->ulCPU0_DTAG = 0x3;
+			__asm__("dsb");
+			
+			/* memory access should trigger an ECC error */
+			ulReadVal = *pulAddr;
+			__asm__("dsb");
+			
+			/* reset syndrome bits */
+			ptCA9_EccCtrl->ulCPU0_DDATA = 0;
+			ptCA9_EccCtrl->ulCPU0_DTAG = 0;
+			__asm__("dsb");
+			
+			/* check for ECC error */
+			ca9_ecc_ctrl_check_print();
+			iRes = (ptCA9_EccCtrl->ulCPU0_SBE_INT != 0) || (ptCA9_EccCtrl->ulCPU0_DBE_INT != 0);
+			
+			uprintf("CA9 core 0 addr: 0x%08x  written: 0x%08x  read: 0x%08x\n", ulAddr, ulWriteVal, ulReadVal);
+
+			/* clear error flags */
+			uprintf("Clearing CA9 core0 L1 ECC error flags after test\n");
+			ptCA9_EccCtrl->ulCPU0_SBE_INT = ptCA9_EccCtrl->ulCPU0_SBE_INT;
+			ptCA9_EccCtrl->ulCPU0_DBE_INT = ptCA9_EccCtrl->ulCPU0_DBE_INT;
+			__asm__("dsb");
+			if ((ptCA9_EccCtrl->ulCPU0_SBE_INT != 0) || (ptCA9_EccCtrl->ulCPU0_DBE_INT != 0))
+			{
+				uprintf("Could not clear CA9 core 0 L1 ECC error flags after test\n");
+				iRes = 0;
+			}
+		}
+	}
+	else
+	{
+		uprintf("Clearing CA9 core1 L1 ECC error flags before test\n");
+		ptCA9_EccCtrl->ulCPU1_SBE_INT = ptCA9_EccCtrl->ulCPU1_SBE_INT;
+		ptCA9_EccCtrl->ulCPU1_DBE_INT = ptCA9_EccCtrl->ulCPU1_DBE_INT;    
+		__asm__("dsb");
+		
+		if ((ptCA9_EccCtrl->ulCPU1_SBE_INT != 0) || (ptCA9_EccCtrl->ulCPU1_DBE_INT != 0))
+		{
+			uprintf("Could not clear CA9 core 1 L1 ECC error flags before test\n");
+			iRes = 0;
+		}
+		else
+		{
+		
+			ptCA9_EccCtrl->ulCPU1_DDATA = 0x3;
+			//ptCA9_EccCtrl->ulCPU1_DTAG = 0x3;
+			__asm__("dsb");
+			
+			ulReadVal = *pulAddr;
+			__asm__("dsb");
+			
+			ptCA9_EccCtrl->ulCPU1_DDATA = 0;
+			ptCA9_EccCtrl->ulCPU1_DTAG = 0;
+			__asm__("dsb");
+			
+			ca9_ecc_ctrl_check_print();
+			iRes = (ptCA9_EccCtrl->ulCPU1_SBE_INT != 0) || (ptCA9_EccCtrl->ulCPU1_DBE_INT != 0);
+			
+			uprintf("CA9 core 1 addr: 0x%08x  written: 0x%08x  read: 0x%08x\n", ulAddr, ulWriteVal, ulReadVal);
+			
+			uprintf("Clearing CA9 core1 L1 ECC error flags after test\n");
+			ptCA9_EccCtrl->ulCPU1_SBE_INT = ptCA9_EccCtrl->ulCPU1_SBE_INT;
+			ptCA9_EccCtrl->ulCPU1_DBE_INT = ptCA9_EccCtrl->ulCPU1_DBE_INT;    
+			__asm__("dsb");
+			if ((ptCA9_EccCtrl->ulCPU1_SBE_INT != 0) || (ptCA9_EccCtrl->ulCPU1_DBE_INT != 0))
+			{
+				uprintf("Could not clear CA9 core 1 L1 ECC error flags after test\n");
+				iRes = 0;
+			}
+		}
+	}
+	
 	
 	if (iRes == 0)
 	{   
 		uprintf("*****************************\n");
-		uprintf("CA9 L1 ECC: ECC check failed!\n");
+		uprintf("CA9 core %d: L1 cache ECC error test FAILED!\n", uiCPUNo);
 		uprintf("*****************************\n");
+		uprintf("\n\n");
+	}
+	else
+	{   
+		uprintf("*****************************\n");
+		uprintf("CA9 core %d: L1 cache ECC error test SUCCEEDED!\n", uiCPUNo);
+		uprintf("*****************************\n");
+		uprintf("\n\n");
 	}
 	
 	return iRes;
 }
+
 
 /* L2C ECC check: WIP */
 int ca9_l2_ecc_ctrl_trigger_errors(void);
