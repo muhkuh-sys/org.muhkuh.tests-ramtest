9f747e508fb2d5874814bc6b00346ef877288026
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2021-10-13 16:46:43 +0200

    Cleanup finalizer. Move the ramtest_cli artifacts to a separate folder.

--- finalizer.lua
+++ finalizer.lua
@@ -1,14 +1,7 @@
 local t = ...
-local strDistId, strDistVersion, strCpuArch = t:get_platform()
-local cLog = t.cLog
-local tLog = t.tLog
-local tResult
-local archives = require 'installer.archives'
-local pl = require'pl.import_into'()
-
 
 -- Copy all additional files.
-local atScripts = {
+t:install{
   -- Install the CLI init script.
   ['local/muhkuh_cli_init.lua'] = '${install_base}/',
 
@@ -17,50 +10,14 @@ local atScripts = {
 
   ['${report_path}']                        = '${install_base}/.jonchki/'
 }
-for strSrc, strDst in pairs(atScripts) do
-  t:install(strSrc, strDst)
+
+t:createPackageFile()
+t:createHashFile()
+-- Does the output path exist?
+local strOutputPath = t:replace_template('${install_base}/../../../ramtest_cli')
+if t.pl.path.exists(strOutputPath)~=strOutputPath then
+  t.pl.path.mkdir(strOutputPath)
 end
+t:createArchive('${install_base}/../../../ramtest_cli/${default_archive_name}', 'native')
 
-
--- Create the package file.
-local strPackageText = t:replace_template([[PACKAGE_NAME=${root_artifact_artifact}
-PACKAGE_VERSION=${root_artifact_version}
-PACKAGE_VCS_ID=${root_artifact_vcs_id}
-HOST_DISTRIBUTION_ID=${platform_distribution_id}
-HOST_DISTRIBUTION_VERSION=${platform_distribution_version}
-HOST_CPU_ARCHITECTURE=${platform_cpu_architecture}
-]])
-local strPackagePath = t:replace_template('${install_base}/.jonchki/package.txt')
-local tFileError, strError = pl.utils.writefile(strPackagePath, strPackageText, false)
-if tFileError==nil then
-  tLog.error('Failed to write the package file "%s": %s', strPackagePath, strError)
-else
-  local Archive = archives(cLog)
-
-  -- Create a ZIP archive for Windows platforms. Build a "tar.gz" for Linux.
-  local strArchiveExtension
-  local tFormat
-  local atFilter
-  if strDistId=='windows' then
-    strArchiveExtension = 'zip'
-    tFormat = Archive.archive.ARCHIVE_FORMAT_ZIP
-    atFilter = {}
-  else
-    strArchiveExtension = 'tar.gz'
-    tFormat = Archive.archive.ARCHIVE_FORMAT_TAR_GNUTAR
-    atFilter = { Archive.archive.ARCHIVE_FILTER_GZIP }
-  end
-
-  local strArtifactVersion = t:replace_template('${root_artifact_artifact}-${root_artifact_version}')
-  local strDV = '-' .. strDistVersion
-  if strDistVersion=='' then
-    strDV = ''
-  end
-  local strArchive = t:replace_template(string.format('${install_base}/../../../%s-%s%s_%s.%s', strArtifactVersion, strDistId, strDV, strCpuArch, strArchiveExtension))
-  local strDiskPath = t:replace_template('${install_base}')
-  local strArchiveMemberPrefix = strArtifactVersion
-
-  tResult = Archive:pack_archive(strArchive, tFormat, atFilter, strDiskPath, strArchiveMemberPrefix)
-end
-
-return tResult
+return true
